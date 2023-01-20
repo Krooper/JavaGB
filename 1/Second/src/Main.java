8107d9f91c8252695c5737dd766f52dba186268c
Krooper <98321262+Krooper@users.noreply.github.com>
2023-01-20 18:32:10 +0300

    made 2nd task

--- 1/Second/src/Main.java
+++ 1/Second/src/Main.java
@@ -1,5 +1,22 @@
+// Вывести все простые числа от 1 до 1000
 public class Main {
-    public static void main(String[] args) {
+    public static boolean checkSimplicity(int number) {
+        if (number <= 3) return true;
+        else if (number % 2 == 0 || number % 3 == 0) return false;
+        int j = 4;
+        while (j < number) {
+            if (number % j == 0) return false;
+            j++;
+        }
+        return true;
+    }
 
+    public static void main(String[] args) {
+        System.out.println("Prime numbers:");
+        for (int i = 2; i <= 1000; i++) {
+            if (checkSimplicity(i)) {
+                System.out.println(i);
+            }
+        }
     }
 }
