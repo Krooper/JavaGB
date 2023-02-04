847637da7f9cd90f1c2ee60b71b61e74739c844b
Krooper <98321262+Krooper@users.noreply.github.com>
2023-02-05 00:10:11 +0300

    made 3.2

--- 3/Second/src/Main.java
+++ 3/Second/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+/**
+ * Пусть дан произвольный список целых чисел, удалить из него четные числа
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> initList = new Random().ints(0, 10).
+                limit(10).boxed().collect(Collectors.toList());
+        System.out.println("Initial List:");
+        System.out.println(initList);
+
+        initList.removeIf(num -> num % 2 == 0);
+
+        System.out.println("List with odd nums only:");
+        System.out.println(initList);
     }
 }
