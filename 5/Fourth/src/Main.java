68714584f86959cadd524bdda988bde729ea3daa
Krooper <98321262+Krooper@users.noreply.github.com>
2023-02-07 19:52:48 +0300

    made 5.4

--- 5/Fourth/src/Main.java
+++ 5/Fourth/src/Main.java
@@ -1,5 +1,33 @@
+/**
+ * Написать метод, который переведёт данное целое число в римский формат.
+ */
 public class Main {
+
+
+    public static String romanizer(int number){
+        int[] nums = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+        String[] romanNums = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+
+        StringBuilder romanNum = new StringBuilder();
+        int i = nums.length - 1;
+        while (number > 0) {
+            while (nums[i] > number){
+                i--;
+            }
+            romanNum.append(romanNums[i]);
+            number -= nums[i];
+        }
+        return romanNum.toString();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int randNumber = (int) (Math.random()*(2000+1));
+        System.out.println("Initial number: ");
+        System.out.println(randNumber);
+
+        System.out.println();
+
+        System.out.println("Roman number: ");
+        System.out.println(romanizer(randNumber));
     }
 }
